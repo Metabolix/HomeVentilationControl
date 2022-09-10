b51ba9ae9fcaa5ab151203a3757d5c94c82dee07
Lauri Kenttä <lauri.kentta@gmail.com>
2022-09-10 15:52:14 +0300

    Add tachy input, calculate RPM.

--- FanController.py
+++ FanController.py
@@ -1,11 +1,54 @@
 from machine import Pin, ADC
+from time import ticks_us, ticks_diff
 from Timestamp import Timestamp
+
+class TachyInput:
+    def __init__(self, pin, timeout):
+        self.timeout = timeout * 1000
+        self.valid_diff = [-1, -1]
+        self.valid_time = [0, 0]
+        self.valid_i = 0
+
+        self.prev_time = ticks_us()
+        self.good_diff = 0
+        self.bad_diff = 0
+        self.bad_count = 0
+        self.pin = Pin(pin, Pin.IN, Pin.PULL_UP)
+        self.pin.irq(lambda p: self._irq(), trigger = Pin.IRQ_FALLING)
+
+    def _irq(self):
+        now = ticks_us()
+        diff = ticks_diff(now, self.prev_time)
+        self.prev_time = now
+        # Reject outliers in data, accept between 80 % and 133 %.
+        if diff * 3 < self.good_diff * 4 < diff * 5:
+            self.good_diff = diff
+            self.valid_diff[self.valid_i ^ 1] = diff
+            self.valid_time[self.valid_i ^ 1] = now
+            self.valid_i = self.valid_i ^ 1
+            self.bad_count = 0
+        elif diff * 2 < self.bad_diff * 3 < diff * 4:
+            self.bad_count += 1
+            # When the same value repeats, accept it eventually.
+            if self.bad_count > 8:
+                self.good_diff = diff
+        else:
+            self.bad_count = 1
+            self.bad_diff = diff
+
+    def diff_us(self):
+        i = self.valid_i
+        diff = self.valid_diff[i]
+        if diff >= self.timeout or ticks_diff(ticks_us(), self.valid_time[i]) >= self.timeout:
+            self.valid_diff[i] = diff = -1
+        return diff
 
 class FanController:
     def __init__(
         self, *,
         pin_switch_on = None, pin_switch_own = None,
         pin_voltage_adc = None, ctrl_translate = None,
+        pin_tachy = None,
     ):
         self.pin_switch_on = pin_switch_on is not None and Pin(pin_switch_on, Pin.IN, pull = Pin.PULL_UP)
         self.pin_switch_own = pin_switch_own is not None and Pin(pin_switch_own, Pin.IN, pull = Pin.PULL_UP)
@@ -16,10 +59,26 @@ class FanController:
         self.ctrl_timestamp = Timestamp(None)
         self.ctrl_rpm = 0
 
+        tachy_off_time = 2_000 # Over 2 seconds = less than 30 rpm will be considered "off".
+        self.tachy_input = pin_tachy is not None and TachyInput(pin_tachy, tachy_off_time)
+        self._rpm_change_timestamp = Timestamp()
+        self._rpm_stable_threshold = 20
+        self.rpm = self._old_rpm = 0
+        self.rpm_stable = False
+
     def update(self):
         self.switch_on = 1 - self.pin_switch_on() if self.pin_switch_on else 0
         self.switch_own = 1 - self.pin_switch_own() if self.pin_switch_own else 0
         self._update_ctrl()
+        self._update_tachy()
+
+    def _update_tachy(self):
+        dt = self.tachy_input and self.tachy_input.diff_us() or -1
+        self.rpm = 60_000_000 // dt if dt > 0 else 0
+        if abs(self.rpm - self._old_rpm) > self._rpm_stable_threshold:
+            self._rpm_change_timestamp = Timestamp()
+        self._old_rpm = ((7 * self._old_rpm) + self.rpm) // 8
+        self.rpm_stable = not self._rpm_change_timestamp.between(0, 5_000)
 
     def _update_ctrl(self):
         mv = 0
--- HomeVentilationControl.py
+++ HomeVentilationControl.py
@@ -22,10 +22,12 @@ class HomeVentilationControl:
         self.c0 = FanController(
             pin_switch_on = 19, pin_switch_own = 22,
             pin_voltage_adc = 28, ctrl_translate = FanController.VilpeECoIdeal,
+            pin_tachy = 16,
         )
         self.c1 = FanController(
             pin_switch_on = 21, pin_switch_own = 20,
             pin_voltage_adc = 27, ctrl_translate = FanController.LapetekVirgola5600XH,
+            pin_tachy = 26,
         )
 
         self._load_conf()
@@ -70,11 +72,11 @@ clock: {clock}
 air: {str_temp_rh(self.air.temperature)} °C, RH {str_temp_rh(self.air.humidity)} %
 
 FAN 0 (main):
-    on {c0.switch_on}, own {c0.switch_own}
+    RPM:  {c0.rpm:4} rpm (on {c0.switch_on}, own {c0.switch_own})
     CTRL: {c0.ctrl_rpm:4} rpm ({c0.ctrl_level[0]} {c0.ctrl_level[1]}, {c0.ctrl_millivolts} mV), age {c0.ctrl_timestamp}
 
 FAN 1 (kitchen hood):
-    on {c1.switch_on}, own {c1.switch_own}
+    RPM:  {c1.rpm:4} rpm (on {c1.switch_on}, own {c1.switch_own})
     CTRL: {c1.ctrl_rpm:4} rpm ({c1.ctrl_level[0]} {c1.ctrl_level[1]}, {c1.ctrl_millivolts} mV), age {c1.ctrl_timestamp}
     IR: speed {self.ir.speed}, age {self.ir.speed_timestamp}
 
