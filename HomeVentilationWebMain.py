d57a1c067371712f9d85642eb0e71953478c042f
Lauri Kenttä <lauri.kentta@gmail.com>
2022-11-09 22:00:45 +0200

    Reboot if network is broken.

--- HomeVentilationWebMain.py
+++ HomeVentilationWebMain.py
@@ -9,6 +9,15 @@ class HomeVentilationWebMain(WebMain):
         network = SimpleWLAN.from_config()
         super().__init__(network)
 
+        def reboot_if_network_is_broken(request):
+            if network.is_broken():
+                self._log("Network is broken.")
+                import machine
+                machine.reset()
+            else:
+                request and request.reply("Not broken.")
+        self.add_module(reboot_if_network_is_broken)
+
         self.add_static("/HomeVentilationWebMain.ico", "/favicon.ico")
         self.add_module(WebFileManager())
 
